1f4ce904ddbbaa611196943800062d960dd63f7c
kasra <kasraalirezaie25@gmail.com>
2026-02-15 12:38:36 +0330

    (feat): random_counter

--- runner.sh
+++ runner.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-ghdl -a --std=08 src/rtl/control_unit/cu.vhd src/tb/control_unit/cu_tb.vhd
-ghdl -e --std=08 cu_tb
-ghdl -r --std=08 cu_tb --wave=cu_wave.ghw --stop-time=200ns
-gtkwave cu_wave.ghw
+ghdl -a --std=08 src/rtl/datapath/random_counter.vhd src/tb/datapath/random_counter_tb.vhd
+ghdl -e --std=08 random_counter_tb
+ghdl -r --std=08 random_counter_tb --wave=random_counter_wave.ghw --stop-time=200ns
+gtkwave random_counter_wave.ghw
